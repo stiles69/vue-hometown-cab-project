7b4e6e4018ab46709bfc79bc3eabd6f15de676f9
stiles69 <brett.salemink@gmail.com>
2018-04-26 00:04:37 -0500

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div id='home'>
-    <!--Begin Cover Image-->
-    <img src='./assets/img/Cover-1920x950.png' class='HometownImage' width='100%' aria-label='Cover Image' />    
-    <!--End Cover Image-->
-    <!--Begin Heading-->
-    <v-layout column align-center>
-    <p class='headline' aria-label='Company Name'>{{ companyname }}</p>
-    <p class='title' aria-label='Company Description'>Newton, Iowa Cab and Taxi Service</p>
-    </v-layout>
-    <!--End Heading-->
-    <!-- Begin Contact -->    
+    <!-- Begin Main Content -->
+      <!--Begin Cover Image-->
+      <img src='./assets/img/Cover-1920x950.png' class='HometownImage' width='100%' aria-label='Cover Image' />    
+      <!--End Cover Image-->
+      <!--Begin Heading-->
+      <v-layout column align-center>
+      <p class='headline' aria-label='Company Name'>{{ companyname }}</p>
+      <p class='title' aria-label='Company Description'>Newton, Iowa Cab and Taxi Service</p>    
+      </v-layout>
+      <!--End Heading-->
+      <!-- Begin Contact -->    
       <v-card id='contact' raised color='white' height='auto'>
       <v-toolbar dark color='primary'>
         <v-toolbar-title class='headline black--text' aria-label='Toolbar Contact'>
@@ -29,123 +30,122 @@
         </p>
       </v-card-text>
       </v-card>    
-    <!-- End Contact -->
-    <!-- Begin Hours -->
-    <v-card id='hours' raised color='white' height='auto'>
-    <v-toolbar dark color='primary' aria-label='Toolbar Hours of Operation'>
-      <v-toolbar-title class='headline black--text'>
-        Hours of Operation
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card-title class='title'>
-      {{ Hours1 }}
-    </v-card-title>
-    <v-card-title class='title'>
-      {{ Hours2 }}
-    </v-card-title>    
+      <!-- End Contact -->
+      <!-- Begin Hours -->
+      <v-card id='hours' raised color='white' height='auto'>
+      <v-toolbar dark color='primary' aria-label='Toolbar Hours of Operation'>
+        <v-toolbar-title class='headline black--text'>
+          Hours of Operation
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card-title class='title'>
+        {{ Hours1 }}
+      </v-card-title>
+      <v-card-title class='subheading'>
+        {{ Hours2 }}
+      </v-card-title>
+        <v-divider></v-divider>
+      <v-card-title class='title'>
+        {{ Hours3 }}
+      </v-card-title>
+      <v-card-title class='subheading'>
+        {{ Hours4 }}
+      </v-card-title>
       <v-divider></v-divider>
-    <v-card-title class='title'>
-      {{ Hours3 }}
-    </v-card-title>
-    <v-card-title class='title''>
-      {{ Hours4 }}
-    </v-card-title>
-    <v-divider></v-divider>
-    
-    <v-card-title class='title Italic'>
-      Open 7 Days a Week
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-card-title class='title Italic'>
-      We take advance reservations for those with Late/Early work schedules.
-    </v-card-title>
-
-    </v-card>
-    <!-- End Hours -->
-    <!-- Begin Pricing -->
-    <v-card id='pricing' raised color='white' height='auto'>
-    <v-toolbar dark color='primary'>
-      <v-toolbar-title class='headline black--text' aria-label='Toolbar Pricing'>
-        Pricing
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card-title class='title'>
-      {{ oneWayPrice }}
-    </v-card-title>
-    <v-card-title class='title'>
-      {{ roundTripPrice }}
-    </v-card-title>
-    <v-card-title class='title'>
-      {{ additionalStopPrice }}
-    </v-card-title>
-    <hr>
-    <v-card-title class='title Italic'>
-      *With a wait time of up to 15 minutes. ($3 Additional charge if over 15 minutes.)
-    </v-card-title>
-    <v-card-title class='title'>
-      Call dispatch for out of town pricing and availability.
-    </v-card-title>
-    <v-card-title class='title'>
-      Keep in mind we offer multiple specials for weekly rates & discount coupons that you can ask your driver about or dispatch.
-    </v-card-title>
-    <hr>
-    <v-card-title class='title Italic'>
-      So far it's been a great experience & we thank you all for your support & hope you keep us in mind for your hometown cab needs.
-    </v-card-title>
-    </v-card>
-    <!-- End Pricing -->
-    <!-- Begin Specials -->
-    <v-card id='specials' raised color='white' height='auto'>
-    <v-toolbar dark color='primary' aria-label='Toolbar Specials'>
-      <v-toolbar-title class='headline black--text'>
-        Specials
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card-title class='title'>
-      We offer discounts for Seniors and Veterans.
-    </v-card-title>
-    <v-card-title class='title'>
-      <div>Weekly Discounts and Specials (See our <span>
-      <a rel='parent' href='https://www.facebook.com/Hometown-Cab-141349606414372/' target='_blank'>Facebook Page</a>)</span>
-      </div>
-    </v-card-title>
-    </v-card>
-    <!-- End Specials -->
-    <!-- Begin Testimonials -->
-    <v-card id='testimonials' raised color='white' class='HometownTestimonials'>
-    <v-toolbar dark color='primary' aria-label='Toolbar Testimonials'>
-      <v-toolbar-title class='headline black--text'>
-        Testimonials
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card-title class='title Italic HometownTestimonials'>
-      Kelly Jo Zach
-    </v-card-title>
-    <v-card-text class='blockquote Italic HometownTestimonialContent HometownTestimonialsSmall'>
-    Hometown Cab was my 'go to' cab, as I was searching for a solution to an issue with my license and a car needing lots of mechanical work. I contacted Bradd, and we agreed on an amount that worked for my budget and was fair to all parties involved. Hometown Cab is very good at offering monthly specials. They also cater to the shifts that manufacturers and businesses
-                          schedule that may not fit within normal work hours.
-    The kindness I receive from Bradd and Chad has been very much appreciated. They always go above and beyond expectations that I had for a cab service. They assist elderly people to door with their groceries, or purchases. They truly
-                          care about their riders.
-    The cabs are mechanically safe, very recognizable, and most importantly, they offer the BEST customer service. I have overheard Bradd talk to other clients. He educates clients very well on expectations and teaching the courtesies
-                          of riding in a cab. He genuinely is concerned if someone may be running late, and he does everything to make sure he is getting the rider to their work, timely, and to their medical appointments timely.
-    Chad is quiet and kind. He has a strong professional personality. He cares greatly about the elderly clients, that their every concern is met with a smile and impeccable service.
-          This cab service has set the bar very high. Bradd and Chad, not only offer safe transportation but excel in service, cleanliness, and compliance.
-    </v-card-text>
-    <v-card-text class='blockquote Italic HometownTestimonialContent HometownTestimonials'>
-    Hometown Cab was my 'go to' cab, as I was searching for a solution to an issue with my license and a car needing lots of mechanical work. I contacted Bradd, and we agreed on an amount that worked for my budget and was fair to all parties involved. Hometown Cab is very good at offering monthly specials. They also cater to the shifts that manufacturers and businesses
-                          schedule that may not fit within normal work hours.
-    The kindness I receive from Bradd and Chad has been very much appreciated. They always go above and beyond expectations that I had for a cab service. They assist elderly people to door with their groceries, or purchases. They truly
-                          care about their riders.
-    The cabs are mechanically safe, very recognizable, and most importantly, they offer the BEST customer service. I have overheard Bradd talk to other clients. He educates clients very well on expectations and teaching the courtesies
-                          of riding in a cab. He genuinely is concerned if someone may be running late, and he does everything to make sure he is getting the rider to their work, timely, and to their medical appointments timely.
-    Chad is quiet and kind. He has a strong professional personality. He cares greatly about the elderly clients, that their every concern is met with a smile and impeccable service.
-          This cab service has set the bar very high. Bradd and Chad, not only offer safe transportation but excel in service, cleanliness, and compliance.
-    </v-card-text>
-    <v-card-title class='title Italic HometownTestimonials'>
-      Kudos. HOMETOWN CAB!!!!
-    </v-card-title>
-    </v-card>
-    <!-- End Testimonials -->
+      <v-card-title class='subheading Italic'>
+        Open 7 Days a Week
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-title class='subheading Italic'>
+        We take advance reservations for those with Late/Early work schedules.
+      </v-card-title>
+      </v-card>
+      <!-- End Hours -->
+      <!-- Begin Pricing -->
+      <v-card id='pricing' raised color='white' height='auto'>
+      <v-toolbar dark color='primary'>
+        <v-toolbar-title class='headline black--text' aria-label='Toolbar Pricing'>
+          Pricing
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card-title class='subheading'>
+        {{ oneWayPrice }}
+      </v-card-title>
+      <v-card-title class='subheading'>
+        {{ roundTripPrice }}
+      </v-card-title>
+      <v-card-title class='subheading'>
+        {{ additionalStopPrice }}
+      </v-card-title>
+      <hr>
+      <v-card-title class='subheading Italic'>
+        *With a wait time of up to 15 minutes. ($3 Additional charge if over 15 minutes.)
+      </v-card-title>
+      <v-card-title class='subheading'>
+        Call dispatch for out of town pricing and availability.
+      </v-card-title>
+      <v-card-title class='subheading'>
+        Keep in mind we offer multiple specials for weekly rates & discount coupons that you can ask your driver about or dispatch.
+      </v-card-title>
+      <hr>
+      <v-card-title class='subheading Italic'>
+        So far it's been a great experience & we thank you all for your support & hope you keep us in mind for your hometown cab needs.
+      </v-card-title>
+      </v-card>
+      <!-- End Pricing -->
+      <!-- Begin Specials -->
+      <v-card id='specials' raised color='white' height='auto'>
+      <v-toolbar dark color='primary' aria-label='Toolbar Specials'>
+        <v-toolbar-title class='headline black--text'>
+          Specials
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card-title class='subheading'>
+        We offer discounts for Seniors and Veterans.
+      </v-card-title>
+      <v-card-title class='subheading'>
+        <div>Weekly Discounts and Specials (See our <span>
+        <a href='https://www.facebook.com/Hometown-Cab-141349606414372/' target='_blank' rel='noopener' class='HometownLink'>Facebook Page</a>)</span>
+        </div>
+      </v-card-title>
+      </v-card>
+      <!-- End Specials -->
+      <!-- Begin Testimonials -->
+      <v-card id='testimonials' raised color='white' class='HometownTestimonials'>
+      <v-toolbar dark color='primary' aria-label='Toolbar Testimonials'>
+        <v-toolbar-title class='headline black--text'>
+          Testimonials
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card-title class='subheading Italic HometownTestimonials'>
+        Kelly Jo Zach
+      </v-card-title>
+      <v-card-text class='blockquote Italic HometownTestimonialContent HometownTestimonialsSmall'>
+      Hometown Cab was my 'go to' cab, as I was searching for a solution to an issue with my license and a car needing lots of mechanical work. I contacted Bradd, and we agreed on an amount that worked for my budget and was fair to all parties involved. Hometown Cab is very good at offering monthly specials. They also cater to the shifts that manufacturers and businesses
+                            schedule that may not fit within normal work hours.
+      The kindness I receive from Bradd and Chad has been very much appreciated. They always go above and beyond expectations that I had for a cab service. They assist elderly people to door with their groceries, or purchases. They truly
+                            care about their riders.
+      The cabs are mechanically safe, very recognizable, and most importantly, they offer the BEST customer service. I have overheard Bradd talk to other clients. He educates clients very well on expectations and teaching the courtesies
+                            of riding in a cab. He genuinely is concerned if someone may be running late, and he does everything to make sure he is getting the rider to their work, timely, and to their medical appointments timely.
+      Chad is quiet and kind. He has a strong professional personality. He cares greatly about the elderly clients, that their every concern is met with a smile and impeccable service.
+            This cab service has set the bar very high. Bradd and Chad, not only offer safe transportation but excel in service, cleanliness, and compliance.
+      </v-card-text>
+      <v-card-text class='blockquote Italic HometownTestimonialContent HometownTestimonials'>
+      Hometown Cab was my 'go to' cab, as I was searching for a solution to an issue with my license and a car needing lots of mechanical work. I contacted Bradd, and we agreed on an amount that worked for my budget and was fair to all parties involved. Hometown Cab is very good at offering monthly specials. They also cater to the shifts that manufacturers and businesses
+                            schedule that may not fit within normal work hours.
+      The kindness I receive from Bradd and Chad has been very much appreciated. They always go above and beyond expectations that I had for a cab service. They assist elderly people to door with their groceries, or purchases. They truly
+                            care about their riders.
+      The cabs are mechanically safe, very recognizable, and most importantly, they offer the BEST customer service. I have overheard Bradd talk to other clients. He educates clients very well on expectations and teaching the courtesies
+                            of riding in a cab. He genuinely is concerned if someone may be running late, and he does everything to make sure he is getting the rider to their work, timely, and to their medical appointments timely.
+      Chad is quiet and kind. He has a strong professional personality. He cares greatly about the elderly clients, that their every concern is met with a smile and impeccable service.
+            This cab service has set the bar very high. Bradd and Chad, not only offer safe transportation but excel in service, cleanliness, and compliance.
+      </v-card-text>
+      <v-card-title class='subheading Italic HometownTestimonials'>
+        Kudos. HOMETOWN CAB!!!!
+      </v-card-title>
+      </v-card>
+      <!-- End Testimonials -->
+      <!-- End Main Content -->
   </div>
 </template>
 
