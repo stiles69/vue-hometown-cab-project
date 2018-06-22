4eeb0e2693c0c54aa7426b69ec4e481d342b9323
stiles69 <brett.salemink@gmail.com>
2018-06-21 21:09:06 -0500

    Changed Hours

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,8 +6,8 @@ const onewayprice = '$7 One-Way (Local)'
 const roundtripprice = '$12 Roundtrip (Local)*'
 const additionalstopprice = '$2 For Each Additional Stop (Local)*'
 const companyslogan = "We're Taking you Places"
-const hours = [ 'Sunday - Wednesday', '5:30 AM - 12:00 AM', 'Thursday - Saturday', '5:30 AM - 2:30 AM' ]
-const version = '202'
+const hours = [ 'Sunday - Thursday', '5:30 AM - 12:00 AM', 'Friday - Saturday', '5:30 AM - 2:30 AM' ]
+const version = '203'
 
 export default {
   companyname, telnum1, telnum2, ownername, onewayprice, roundtripprice, additionalstopprice, companyslogan, hours, version
