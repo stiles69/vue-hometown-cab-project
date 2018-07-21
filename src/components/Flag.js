145f97c93e753f0a04ed2de32f7b64d50cb1ffab
stiles69 <brett.salemink@gmail.com>
2018-07-21 06:13:15 -0500

    Added fontawesome

---
+++ src/components/Flag.js
@@ -0,0 +1,15 @@
+import Fa from 'vue-fa'
+import { faFlag } from '@fortawesome/fontawesome-free-solid'
+
+export default {
+	components: {
+		Fa
+	},
+
+	data () {
+		return {
+			faFlag
+			}
+		}
+	}
+
